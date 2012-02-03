5603c90a9c004e02d01361f8c2cf8ebb08758a99
Anatoly Kanivetsky <Chaos.A.D88@gmail.com>
2012-02-03 12:39:32 +0400

    forgot to export generate_auth from social_net_api_client.erl

--- src/social_net_api_client.erl
+++ src/social_net_api_client.erl
@@ -23,7 +23,7 @@
 
 -export([start_link/0]).
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, code_change/3, terminate/2]).
--export([validate_auth/1, invoke_method/2, send_message/2, get_currency_multiplier/0]).
+-export([generate_auth/1, validate_auth/1, invoke_method/2, send_message/2, get_currency_multiplier/0]).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
